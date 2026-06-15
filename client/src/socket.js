35b1ad0d7765e642c4ad6d753360fc1dd12248ff
roodriii17 <161836985+roodriii17@users.noreply.github.com>
2026-06-15 09:55:29 +0200

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Apuntamos a la IP de tu PC, pero al puerto 3003 (donde está el backend escuchando)
-const socket = io("http://192.168.102.27:3003", {
+const socket = io("https://whatsapprec-one.vercel.app", {
   autoConnect: false 
 });
 
